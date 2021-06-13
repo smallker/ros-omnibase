687e63883fa5df994cfa78ca58b2b8f086ff5eaa
unknown <wahyu.smallker@gmail.com>
2021-06-14 06:26:57 +0700

    fix rumus odometry

--- catkin_ws/src/omnibot/src/omnibot/kinematics.py
+++ catkin_ws/src/omnibot/src/omnibot/kinematics.py
@@ -8,7 +8,7 @@ class Kinematics:
         motor = MotorSpeed()
         x = speed.linear.x
         y = speed.linear.y
-        w = 0
+        w = speed.angular.z
         motor.a = (0.58 * x) + (-0.33 * y) + (0.33 * w)
         motor.b = (-0.58 * x) + (-0.33 * y) + (0.33 * w)
         motor.c = (0 * x) + (0.67 * y) + (0.33 * w)
--- catkin_ws/src/omnibot/src/omnibot/odometry.py
+++ catkin_ws/src/omnibot/src/omnibot/odometry.py
@@ -27,9 +27,11 @@ class Odometry:
 
     def update_pose(self, new_time):
         delta_time = new_time - self.last_time
-        self.pose.y = (sqrt3 * self.m_a) - (sqrt3 * self.m_b)
-        self.pose.x = -1*((2 * self.m_c) - self.m_a - self.m_b)
-        self.pose.theta = (self.m_a + self.m_c + self.m_b) / self.base_wheel
+        # self.pose.y = (sqrt3 * self.m_a) - (sqrt3 * self.m_b)
+        # self.pose.x = -1*((2 * self.m_c) - self.m_a - self.m_b)
+        self.pose.y = ((sqrt3 * self.m_b) - (sqrt3 * self.m_a))/ -3
+        self.pose.x = ((2*self.m_c) - self.m_a - self.m_b)/ -3
+        self.pose.theta = -1 * ((self.m_a + self.m_c + self.m_b) / (self.base_wheel * 3))
         self.pose.xVel = abs((self.pose.x - self.last_pose.x) / delta_time)
         self.pose.yVel = abs((self.pose.y - self.last_pose.y)/ delta_time)
         self.pose.thetaVel = abs((self.pose.theta - self.last_pose.thetaVel)/delta_time)
