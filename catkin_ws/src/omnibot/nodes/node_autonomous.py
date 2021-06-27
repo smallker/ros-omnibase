a8a65a3d74ee21d44b7c9e7f7ce53c4a901ceaa2
Wahyu Hidayat <wahyu.smallker@gmail.com>
2021-06-28 00:33:04 +0700

    kalibrasi PID autonomous drive

--- catkin_ws/src/omnibot/nodes/node_autonomous.py
+++ catkin_ws/src/omnibot/nodes/node_autonomous.py
@@ -3,7 +3,7 @@ import rospy
 from std_msgs.msg import Int32, Empty
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, Point
 from omnibot.pid import Pid
 
 
@@ -11,9 +11,9 @@ class NodePlay:
     # f = open('log.txt')
     position = 0
     finish = False
-    pid_x = Pid(0.5, 0, 0)
-    pid_y = Pid(0.5, 0, 0)
-    pid_z = Pid(1, 0, 0)
+    pid_x = Pid(0.5, 0, 0.2)
+    pid_y = Pid(0.5, 0, 0.2)
+    pid_z = Pid(0.8, 0, 0.25)
     twist = Twist()
 
     def odom_callback(self, msg_data: Odometry):
@@ -24,10 +24,10 @@ class NodePlay:
         if self.finish is not True:
             self.twist.linear.x = self.pid_y.pid()
             self.twist.linear.y = - self.pid_x.pid()
-            self.twist.angular.z = self.pid_z.pid()
+            self.twist.angular.z = - self.pid_z.pid()
             # rospy.loginfo(f'zErr : {self.pid_z.last_err} pid : {self.twist.angular.z}')
             self.cmd_vel.publish(self.twist)
-        if abs(self.pid_x.sp - self.pid_x.pos) < 0.05 and abs(self.pid_y.sp - self.pid_y.pos) < 0.05:
+        if abs(self.pid_x.sp - self.pid_x.pos) < 0.01 and abs(self.pid_y.sp - self.pid_y.pos) < 0.01 and abs(self.pid_z.sp - self.pid_z.pos) < 0.01:
             self.position += 1
             if self.position < self.arr.__len__():
                 self.pid_x.reset_err()
@@ -47,6 +47,10 @@ class NodePlay:
         self.pid_x.reset_err()
         self.pid_y.reset_err()
         self.pid_z.reset_err()
+        self.twist.linear.x = 0
+        self.twist.linear.y = 0
+        self.twist.angular.z = 0
+        self.cmd_vel.publish(self.twist)
         self.finish = True
 
     def __setpoint(self, msg:PoseStamped):
@@ -62,6 +66,14 @@ class NodePlay:
     def __cmp_callback(self, msg:Int32):
         self.pid_z.pos = msg.data * pi / 180
 
+
+    def __set_pid(self, msg:Point):
+        self.pid_x = Pid(msg.x, msg.y, msg.z)
+        self.pid_y = Pid(msg.x, msg.y, msg.z)
+
+    def __set_angular_pid(self, msg:Point):
+        self.pid_z = Pid(msg.x, msg.y, msg.z)
+
     def __init__(self) -> None:
         # self.arr = self.f.readlines()
         # self.arr = [[1, 0, 1], [0, 0, 1], [0, 1, 1], [0, 0, 1]]
@@ -75,6 +87,8 @@ class NodePlay:
         rospy.Subscriber('/sensor/compass', Int32, self.__cmp_callback)
         rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.__setpoint)
         rospy.Subscriber('/reset_pos', Empty, self.__reset)
+        rospy.Subscriber('/set_pid', Point, self.__set_pid)
+        rospy.Subscriber('/set_angular_pid', Point, self.__set_angular_pid)
         self.cmd_vel = rospy.Publisher('cmd_vel', Twist, queue_size=1)
         
         # for i in range(arr.__len__()):
